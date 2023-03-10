d72275102c37ed8cd92a0fe14483cb2f76b2b2d5
Halcyon1902 <70284297+halcyon1902@users.noreply.github.com>
2023-03-10 23:45:33 +0700

    project vừa được tạo

---
+++ webquiz/src/test/java/congcu/nhom9/webquiz/WebquizApplicationTests.java
@@ -0,0 +1,13 @@
+package congcu.nhom9.webquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
