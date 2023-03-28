ec63110ca3c502f1d2baf202fd8029919fe9a363
caonguyen21 <106511965+caonguyen21@users.noreply.github.com>
2023-03-28 15:50:15 +0700

    1.1
    Admin:CRUD sản phẩm

--- src/main/java/DiamongSpringMVC/Controller/Admin/AdminViewController.java
+++ src/main/java/DiamongSpringMVC/Controller/Admin/AdminViewController.java
@@ -1,5 +1,9 @@
 package DiamongSpringMVC.Controller.Admin;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -8,18 +12,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
-
+import DiamongSpringMVC.MySQL.Repo.*;
+import DiamongSpringMVC.Entity.*;
 import DiamongSpringMVC.Entity.Category;
-
-import DiamongSpringMVC.MySQL.Repo.CategoryRepository;
-
+import DiamongSpringMVC.MySQL.Model.*;
 import DiamongSpringMVC.Entity.User;
 import DiamongSpringMVC.MySQL.Repo.UserRepository;
 
@@ -29,6 +34,7 @@ public class AdminViewController {
 
 	private @Autowired UserRepository userRepository;
 	private @Autowired CategoryRepository categoryRepository;
+	private @Autowired ProductRepository productRepository;
 	/*
 	 * private @Autowired CategoryRepository categoryRepository; private @Autowired
 	 * ProductRepository productRepository; private @Autowired BillRepository
@@ -128,5 +134,84 @@ public class AdminViewController {
 			System.out.println(e);
 		}
 		return "redirect:" + url;
+	}
+
+	@GetMapping("/manager_product")
+	public ModelAndView product() {
+		ModelAndView mv = new ModelAndView("admin/manager_product");
+		mv.addObject("prods", productRepository.findAll());
+		return mv;
+	}
+
+	@GetMapping("/product/add")
+	public ModelAndView productAdd() {
+		ModelAndView mv = new ModelAndView("admin/insert_product");
+		mv.addObject("cates", categoryRepository.findAll());
+		return mv;
+	}
+
+	@GetMapping("/product/update_product/{id}")
+	public ModelAndView productUpdate(@PathVariable Long id) {
+		ModelAndView mv = new ModelAndView("admin/update_product");
+		Product p = productRepository.findById(id).get();
+		mv.addObject("cates", categoryRepository.findAll());
+		mv.addObject("product", p);
+		mv.addObject("cate", categoryRepository.findById(p.getCategoryId()).get());
+		return mv;
+	}
+
+	@PostMapping("/product/manager_product")
+	public @ResponseBody CustomUrl prodUpdate(@RequestBody ProductDto dto, HttpServletRequest request) {
+		String error = "";
+		if (!StringUtils.isEmpty(dto.getProdName())) {
+			error = "Vui lòng nhập tên sản phẩm!";
+			request.setAttribute("error", error);
+		}
+
+		Product p = new Product(dto.getCateId(), dto.getProdName(), "/resources/images/" + dto.getImage(),
+				dto.getPrice(), dto.getDescrip());
+		p.setProductBuy(dto.getBuy());
+		p.setProductProvider(dto.getProvider());
+		p.setProductQuantity(dto.getQuantity());
+		CustomUrl url = new CustomUrl();
+		try {
+			switch (dto.getCommand()) {
+			case "insert":
+				productRepository.save(p);
+				url.setUrl("/admin/manager_product");
+				break;
+			case "update":
+				p.setProductID(dto.getProdId());
+				productRepository.save(p);
+				url.setUrl("/admin/manager_product");
+				break;
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return url;
+	}
+
+	@PostMapping("/product/file")
+	public @ResponseBody String uploadProductFile(@ModelAttribute MultipartFile hinhanh, HttpServletRequest request) {
+		ServletContext servletContext = request.getServletContext();
+		String contextPath = servletContext.getRealPath("/");
+		String filename = contextPath + "/resources/images/" + hinhanh.getOriginalFilename();
+
+		try {
+			hinhanh.transferTo(new File(filename));
+		} catch (IllegalStateException e1) {
+			System.out.println(e1);
+		} catch (IOException e1) {
+			System.out.println(e1);
+		}
+
+		return hinhanh.getOriginalFilename();
+	}
+
+	@GetMapping("/product/delete/{id}")
+	public String productDelete(@PathVariable Long id) {
+		productRepository.deleteById(id);
+		return "redirect:/admin/manager_product";
 	}
 }
