b973679110d481a487e884aa53b2a8349fdc84eb
BK-SilverBullet <106644883+BK-SilverBullet@users.noreply.github.com>
2023-03-28 17:37:11 +0700

    1.0
    upload file service

---
+++ src/main/java/DiamongSpringMVC/service/SendmailService.java
@@ -0,0 +1,7 @@
+/***************************************************************************
+ * Copyright 2016 by HomeDirect - All rights reserved.                *    
+ **************************************************************************/
+package com.webstore.app.service;
+
+public class SendmailService {
+}
