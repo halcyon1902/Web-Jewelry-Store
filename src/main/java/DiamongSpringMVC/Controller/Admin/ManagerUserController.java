1d23e90d2cb935d4405708c517598ef344d440fc
Halcyon1902 <70284297+halcyon1902@users.noreply.github.com>
2023-03-28 17:29:44 +0700

    thêm tạo user

---
+++ src/main/java/DiamongSpringMVC/Controller/Admin/ManagerUserController.java
@@ -0,0 +1,42 @@
+package com.webstore.app.adm.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.webstore.app.entity.User;
+import com.webstore.app.mysql.repo.UserRepository;
+
+@Controller
+@RequestMapping("/user")
+public class ManagerUserController {
+
+  private @Autowired UserRepository userRepository;
+  private final static int customer_group = 1;
+  
+  
+  @PostMapping("/login")
+  protected String userlogin(@RequestParam String command, @RequestParam String email, @RequestParam(required = false) String phone, @RequestParam String password, HttpServletRequest request) {
+      String url = "";
+      User users = new User();
+      HttpSession session = request.getSession();
+      switch (command) {
+          case "insert":
+              users.setUserEmail(email);
+              users.setUserPass(password);
+              users.setUserRole(false);
+              users.setGroupid(customer_group);
+              users.setPhone(phone);
+              userRepository.save(users);
+              session.setAttribute("user", users);
+              url = "shop/homepage";
+              break;
+      }
+      return "redirect:/" + url;
+  }
+}
